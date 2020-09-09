b82653a876ce7671f47627881e5e608e9e4a1ec2
Jovit-Mathew236 <70875875+Jovit-Mathew236@users.noreply.github.com>
2020-09-09 22:50:17 +0530

    Add files via upload

---
+++ myweb.js
@@ -0,0 +1,10 @@
+var colors = ["black", "blue"];
+var colorIndex = 0;
+function changeColor() {
+    var col = document.getElementById("body");
+    if( colorIndex >= colors.length ) {
+        colorIndex = 0;
+    }
+    col.style.backgroundColor = colors[colorIndex];
+    colorIndex++;
+}
