220ac2d6f45d3c6b3a4cedcb280e89f1e813a29d
Jovit-Mathew236 <70875875+Jovit-Mathew236@users.noreply.github.com>
2020-09-17 12:50:05 +0530

    Update myweb.js

--- myweb.js
+++ myweb.js
@@ -1,10 +1,13 @@
-var colors = ["black", "blue"];
-var colorIndex = 0;
-function changeColor() {
-    var col = document.getElementById("body");
-    if( colorIndex >= colors.length ) {
-        colorIndex = 0;
+var colors = ["black", "blue", ];
+    var colorIndex = 0;
+    function changeColor() {
+        var col = document.getElementById("body");
+        if( colorIndex >= colors.length ) {
+            colorIndex = 0;
+        }
+        col.style.backgroundColor = colors[colorIndex];
+        colorIndex++;
     }
-    col.style.backgroundColor = colors[colorIndex];
-    colorIndex++;
-}
+function Alt(){
+    alert("https://jovit-mathew236.github.io/my-personal-web/")
+} 
